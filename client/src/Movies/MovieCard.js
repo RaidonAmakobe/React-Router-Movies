798c1f9cdbdb12d19f6a518c6c797e6e69d5938a
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-05-07 21:14:52 -0400

    MovieCard js

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 const MovieCard = ({ movie }) => {
   const { title, director, metascore, stars, id} = movie;
-  return;
+  return(
+    <div className = "movide-card">
+      <h2>{title}</h2>
+      <div className = "movie-director">
+        BigBoss: <strong>{director}</strong>
+      </div>
+      <div className = "movie-metascore">
+        Meta: <em>{metascore}</em>
+      </div>
+    </div>
+    <h3>Celebs</h3>
+    {stars.map(star => (
+      <div key = {star} className = "movie-star">
+        {star}
+      </div>
+    ))}
+  );
 };
 
 export default MovieCard;
